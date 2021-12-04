0877ea458462467e0f5b3e5789f45c901e335f7e
ebo16@vt.edu <ebo16@vt.edu>
2021-12-04 18:41:26 -0500

    added comment, post, and user apis and dashboard and homepage routes

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
+// Import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//create connection to our database, pass in your MySQL info for username and password
+// Create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -14,13 +15,5 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
 
 module.exports = sequelize;
