3b49b16da00f81a03c36e3d44630f6c405662370
ebo16@vt.edu <ebo16@vt.edu>
2021-12-04 22:45:15 -0500

    restrucure handlebars

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,16 +1,5 @@
-const {format_date, format_plural, format_url} = require('../utils/helpers');
+const {format_date, format_plural} = require('../utils/helpers');
 
-
-//shortens urls
-test('format_url() returns a simplified url string', () => {
-  const url1 = format_url('http://test.com/page/1');
-  const url2 = format_url('https://www.coolstuff.com/abcdefg/');
-  const url3 = format_url('https://www.google.com?q=hello');
-
-  expect(url1).toBe('test.com');
-  expect(url2).toBe('coolstuff.com');
-  expect(url3).toBe('google.com');
-});
 
 //will make plural point and comment depending on count
 test('format_plural() returns a pluralized word', () => {
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,6 +42,44 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
+router.get('/create/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // use the ID from the session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // serialize data before passing to template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('create-post', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 //edit a post
 router.get('/edit/:id', withAuth, (req, res) => {
     Post.findOne({
